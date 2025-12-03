5000ec2b8b7728ae8f95076602e04a54b821d89f
alex-riz-NE <114101331+alex-riz-NE@users.noreply.github.com>
2025-12-03 18:37:07 -0500

    Update observablehq.config.js

--- observable_site/observablehq.config.js
+++ observable_site/observablehq.config.js
@@ -1,7 +1,9 @@
 export default {
   title: "Stroke PIM Study",
+  root: "src",
   pages: [
     { name: "Home", path: "index.md" }
   ],
-  base: "/project-fall25-alex-riz-NE/"
+  base: "/project-fall25-alex-riz-NE/",
+  output: "dist"
 };
